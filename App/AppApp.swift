2808a84c86ba6d1ef423f2cd4cdb0372e8380bc8
lntcu <221248485+lntcu@users.noreply.github.com>
2025-11-08 13:53:05 +0800

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by user on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
